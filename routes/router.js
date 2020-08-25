f29f12f6b0d0c8af68e915ecf10c6cf43588d08d
Matte <51192395+MatteZ02@users.noreply.github.com>
2020-08-25 14:14:13 +0300

    update 25.8

--- routes/router.js
+++ routes/router.js
@@ -1,21 +1,22 @@
 const db = require("../db");
 
 module.exports = (app) => {
-    const renderTemplate = (req, res, template, code) => {
-        res.render(template, { db, code, user: req.session.user });
+    const renderTemplate = (req, res, template, params, code) => {
+        const user = req.session.user || [];
+        res.render(template, { db, params, code, user: user[0]});
     };
 
-    const isAuthenticated = (req, res, template) => {
+    const isAuthenticated = (req, res, template, params) => {
         if (req.session.loggedin) {
-            renderTemplate(req, res, template);
+            renderTemplate(req, res, template, params);
         } else {
             res.redirect("/login");
         }
     }
 
-    const isAuthenticatedInv = (req, res, template) => {
+    const isAuthenticatedInv = (req, res, template, params) => {
         if (!req.session.loggedin) {
-            renderTemplate(req, res, template);
+            renderTemplate(req, res, template, params);
         } else {
             res.redirect("/");
         }
@@ -25,9 +26,15 @@ module.exports = (app) => {
 
     app.get("/login", (req, res) => isAuthenticatedInv(req, res, "login"));
     app.get("/register", (req, res) => isAuthenticatedInv(req, res, "register"));
-    app.get("/drinks", (req, res) => isAuthenticated(req, res, "drinks"));
-    app.get("/add", (req, res) => isAuthenticated(req, res, "add"));
+    app.get("/drinks", async (req, res) => isAuthenticated(req, res, "drinks", { drinks: await db.select({table: "drinks"}) }));
+    app.get("/add", async (req, res) => isAuthenticated(req, res, "add", { ingredients: await db.select({table: "ingredient"}) }));
     app.get("/user", (req, res) => isAuthenticated(req, res, "user"));
+
+    app.get("/drink/:drinkID", async (req, res) => {
+        const id = req.params.drinkID;
+        const drink = await db.select({table: "drinks", where: `id = ${id}`});
+        isAuthenticated(req, res, "drink", { id, drink: drink[0] });
+    });
 
     app.get("/logout", (req, res) => {
         req.session.loggedin = false;
@@ -35,52 +42,64 @@ module.exports = (app) => {
     });
 
     app.post("/registered", async (req, res) => {
-        const email = req.body.email;
-        const username = req.body.username;
-        const password = req.body.password;
-        const password2 = req.body.password2;
-        const results = await db.select({fields: "*", table: "accounts"});
+        const results = await db.select({table: "accounts"});
         let inUse = false;
         results.forEach(item => {
-            if (item.email.toLowerCase() == email.toLowerCase()) {
+            if (item.email.toLowerCase() == req.body.email.toLowerCase()) {
                 if (inUse) return;
                 inUse = true;
-                return renderTemplate(req, res, "register", "EMAIL_INUSE");
-            } else if (item.username.toLowerCase() == username.toLowerCase()) {
+                return renderTemplate(req, res, "register", {}, "EMAIL_INUSE");
+            } else if (item.username.toLowerCase() == req.body.username.toLowerCase()) {
                 if (inUse) return;
                 inUse = true;
-                return renderTemplate(req, res, "register", "USERNAME_INUSE");
+                return renderTemplate(req, res, "register", {}, "USERNAME_INUSE");
             }
         });
         if (inUse) return;
-        if (password !== password2) {
-            return renderTemplate(req, res, "register", "PASSWORD_NOT_MATCH");
+        if (req.body.password !== req.body.password2) {
+            return renderTemplate(req, res, "register", {}, "PASSWORD_NOT_MATCH");
         }
-        if (username && password && password2 && email) {
-            db.insert({table: "accounts", fields: 'username, password, email, admin', values: `"${username}", "${password}", "${email}, 0"`});
-            return renderTemplate(req, res, "login", "REGISTERED");
+        if (req.body.username && req.body.password && req.body.password2 && req.body.email) {
+            db.insert({table: "accounts", fields: "username, password, email, admin", values: `"${req.body.username}", "${req.body.password}", "${req.body.email}", "0"`});
+            return renderTemplate(req, res, "login", {}, "REGISTERED");
         } else {
             res.send('Please fill the requiered fields!');
             res.end();
         }
     });
 
-    app.post('/auth', async (req, res) => {
-        const username = req.body.username;
-        const password = req.body.password;
-        if (username && password) {
-           const results = await db.select({fields: "*", table: "accounts", where: "username = ? AND password = ?", username: username, password: password});
+    app.post("/auth", async (req, res) => {
+        if (req.body.username && req.body.password) {
+           const results = await db.select({table: "accounts", where: "username = ? AND password = ?", username: req.body.username, password: req.body.password});
                 if (results.length > 0) {
+                    if (results.admin == 1) results.admin = true;
+                    else results.admin = false;
                     req.session.loggedin = true;
                     req.session.user = results;
                     res.redirect('/');
                 } else {
-                    renderTemplate(req, res, "login", "INCORRECT_LOGIN_DETAILS");         
+                    renderTemplate(req, res, "login", {}, "INCORRECT_LOGIN_DETAILS");         
                 }			
                 res.end();
         } else {
             res.send('Please enter Username and Password!');
             res.end();
         }
+    });
+
+    app.post("/add", async (req, res) => {
+        const result = await db.insert({table: "drinks", fields: "name, genre, recipe, approved", values: `"${req.body.name}", "${req.body.genre}", "${req.body.recipe}", "0"`});
+        let i = 0;
+        req.body.ingredients.forEach(item => {
+            if (item == 1) return;
+            db.insert({table: "ingredients", fields: "drink, ingredient, amount", values: `"${result.insertId}", "${item}", "${req.body.amount[i]}"`});
+            i++;
+        });
+        res.redirect(`/drink/${result.insertId}`);
+    });
+
+    app.post("/addingredient", (req, res) => {
+        db.insert({table: "ingredient", fields: "name", values: `"${req.body.name}"`});
+        res.redirect("/add");
     });
 }
--- struct/database.js
+++ struct/database.js
@@ -12,6 +12,7 @@ module.exports = class database {
           this.connection.connect();
     }
     select (options) {
+      if (!options.fields) options.fields = "*";
       if (typeof options !== "object") throw new Error(`The options must be of type "object" type received was ${typeof options}`);
       if (typeof options.fields !== "string") throw new Error(`fields must be of type "string" type received was ${typeof options.fields}.`);
       if (typeof options.table !== "string") throw new Error(`table must be of type "string" type received was ${typeof options.table}.`);
@@ -19,20 +20,22 @@ module.exports = class database {
         if (typeof options.where !== "string") throw new Error(`where must be of type "string" type received was ${typeof options.table}.`);
         if (options.username && options.password) {
           return new Promise(resolve => {
-          this.connection.query(`select ${options.fields} from ${options.table} where ${options.where}`, [options.username, options.password], (error, results, fields) => {
+          this.connection.query(`select ${options.fields} from ${options.table} where ${options.where}`, [options.username, options.password], (error, results) => {
+            if (error) throw new Error(error);
             resolve(results);
           });
         });
          } else
         return new Promise(resolve => {
-        this.connection.query(`select ${options.fields} from ${options.table} where ${options.where}`, (error, results, fields) => {
+        this.connection.query(`select ${options.fields} from ${options.table} where ${options.where}`, (error, results) => {
+          if (error) throw new Error(error);
           resolve(results);
         });
       });
-        
       } else {
         return new Promise(resolve => {
-          this.connection.query(`select ${options.fields} from ${options.table}`, (error, results, fields) => {
+          this.connection.query(`select ${options.fields} from ${options.table}`, (error, results) => {
+            if (error) throw new Error(error);
             resolve(results);
           });
         });
@@ -45,8 +48,8 @@ module.exports = class database {
       if (typeof options.fields !== "string") throw new Error(`fields must be of type "string" type received was ${typeof options.fields}.`);
       if (typeof options.values !== "string") throw new Error(`values must be of type "string" type received was ${typeof options.values}.`);
         return new Promise(resolve => {
-          console.log(`insert into ${options.table} (${options.fields}) values (${options.values})`);
-          this.connection.query(`insert into ${options.table} (${options.fields}) values (${options.values})`, (error, results, fields) => {
+          this.connection.query(`insert into ${options.table} (${options.fields}) values (${options.values})`, (error, results) => {
+            if (error) throw new Error(error);
             resolve(results);
           });
         });
